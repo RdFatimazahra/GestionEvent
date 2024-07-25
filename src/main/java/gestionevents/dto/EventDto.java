747e51df37946cf751286d5df541aaeb801f18df
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-07-25 17:08:27 +0100

    Add DtoEvent

---
+++ src/main/java/gestionevents/dto/EventDto.java
@@ -0,0 +1,30 @@
+package gestionevents.dto;
+
+import gestionevents.model.Reservation;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class EventDto {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int idEvent;
+        private String name;
+        private String description;
+        private String date;
+        private String category;
+
+    }
+
+
+
+
+}
---
+++ src/main/java/gestionevents/repository/EventRepository.java
@@ -0,0 +1,4 @@
+package gestionevents.repository;
+
+public interface EventRepository {
+}
