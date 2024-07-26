4ae006b10b8499977f66ad00a97cd38ae94ce3b7
najia <najiaknizi199@gmail.com>
2024-07-26 15:47:48 +0100

    conflict

--- src/main/java/gestionevents/repository/ContactRepository.java
+++ src/main/java/gestionevents/repository/ContactRepository.java
@@ -2,13 +2,8 @@ package gestionevents.repository;
 
 import gestionevents.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-import org.springframework.data.repository.CrudRepository;
+
 
 public interface ContactRepository extends JpaRepository<Contact,Integer >{
-=======
-
-public interface ContactRepository extends JpaRepository<Contact, Integer> {
->>>>>>> f79a85aa5df5bcf321a9b8d030c606f0e9cfa406
 
 }
